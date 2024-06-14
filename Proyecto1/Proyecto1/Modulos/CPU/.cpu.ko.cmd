33d7fed0af935041776060672b12610d98c5c03d
Nahomi-Aparicio <3072986550401@ingenieria.usac.edu.gt>
2024-06-14 02:31:15 +0000

    segundo intento

--- Proyecto1/Proyecto1/Modulos/CPU/.cpu.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nayo/Proyecto/Proyecto1/Modulos/CPU/cpu.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/nayo/Proyecto/Proyecto1/Modulos/CPU/cpu.ko /home/nayo/Proyecto/Proyecto1/Modulos/CPU/cpu.o /home/nayo/Proyecto/Proyecto1/Modulos/CPU/cpu.mod.o;  true
